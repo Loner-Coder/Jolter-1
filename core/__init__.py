55db7428ab0b98d3684acc736145e07065a013ad
@_tID <robinsonpinaxx2000@gmail.com>
2018-04-13 16:17:41 +0530

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding : utf-8 -*-
+#
+##############
+#   Jolter   #
+##############
+#
+#This is the init1 file of Jolter Framework.
+#https://github.com/the-Infected-Drake/Jolter
+
+from impo.breach_mode import *
+from infolist.info_extract import *
+
+pass
